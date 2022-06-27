d0f2cd1236b65a2b6cbf2946118761f5a23968a8
Emran Khandaker Evan <evanemran007@gmail.com>
2022-06-27 16:45:02 +0600

    imagepicker added

--- app/src/main/java/com/evanemran/quickfeed/dialogs/PostDialog.java
+++ app/src/main/java/com/evanemran/quickfeed/dialogs/PostDialog.java
@@ -1,12 +1,20 @@
 package com.evanemran.quickfeed.dialogs;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,11 +24,16 @@ import com.evanemran.quickfeed.R;
 import com.evanemran.quickfeed.listeners.ClickListener;
 import com.evanemran.quickfeed.models.PostData;
 
+import java.io.IOException;
+
 public class PostDialog extends DialogFragment implements ClickListener<PostData> {
 
     private ClickListener<PostData> listener;
     EditText editText_post;
     Button button_post;
+    ImageButton button_image_upload;
+    ImageView imageView_selected;
+    Uri filePath;
 
     public PostDialog(ClickListener<PostData> listener) {
         this.listener = listener;
@@ -43,6 +56,16 @@ public class PostDialog extends DialogFragment implements ClickListener<PostData
 
         editText_post = view.findViewById(R.id.editText_post);
         button_post = view.findViewById(R.id.button_post);
+        button_image_upload = view.findViewById(R.id.button_image_upload);
+        imageView_selected = view.findViewById(R.id.imageView_selected);
+
+
+        button_image_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SelectImage();
+            }
+        });
 
         button_post.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +76,53 @@ public class PostDialog extends DialogFragment implements ClickListener<PostData
                 dismiss();
             }
         });
+    }
+
+    private void SelectImage()
+    {
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                100);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == 100
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore
+                        .Images
+                        .Media
+                        .getBitmap(
+                                getContext().getContentResolver(),
+                                filePath);
+                imageView_selected.setVisibility(View.VISIBLE);
+                imageView_selected.setImageBitmap(bitmap);
+            }
+
+            catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
